e4dee955f4d7eff76c79dfc5595d85e1edab279f
alejo28121 <grajalesvargasalejandro@gmail.com>
2025-05-12 20:55:52 -0500

    90% create user

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,27 @@
+import './CreateUser.css'
+
 function CreateUser(){
     return(
-        <h1>New User</h1>
+        <div className="MainC">
+            <div className="textUserContentC">
+                <h2 className='createAccount'>Crear Usuario</h2>
+            </div>
+            <div className="nameContent">
+                <input className="inputName" placeholder="Nombre"></input>
+            </div>
+            <div className="lastNameContent">
+                <input className="inputLasName" placeholder="Apellido"></input>
+            </div>
+            <div className="codeContent">
+                <input className="inputCode" placeholder="Codigo de vinculación"></input>
+            </div>
+            <div className="passwordContentC">
+                <input className="inputPasswordC" placeholder="Contraseña"></input>
+            </div>
+            <div className="buttonContent">
+                <button className="ButtonIn">Crear</button>
+            </div>
+        </div>
     );
 }
 export default CreateUser;
